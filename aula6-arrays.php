62ce9a3f809e3ea53b3bc12323ec8923568b4279
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2021-12-16 20:07:30 -0300

    ajustes aula 6

---
+++ aula6-arrays.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays</title>
+</head>
+<body>
+    <h3>Arrays em PHP</h3>
+    <p>Array ou Vetor é uma sequência de informações geralmente do mesmo tipo.</p>
+    <?php
+        $listaIdade = array(12, 11, 54, 12);
+        $listaNome = [212, 1212, 2121];
+        $teste = $listaNome[0];
+        echo $teste;
+    ?>
+</body>
+</html>
