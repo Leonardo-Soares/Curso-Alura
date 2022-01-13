65ef4460fe684128cd2cc99d134ac50939fab44b
Leonardo-Soares <leonardoafonso1048@gmail.com>
2022-01-13 08:59:54 -0300

    Conclusão do curso php

--- layouts/rodape.php
+++ layouts/rodape.php
@@ -1,7 +1,10 @@
 
-<footer style="text-align: center;">
-    Aulas sobre PHP Avançado
-</footer>
+    <footer class="text-center fw-bold text-uppercase ">
+        Aulas sobre PHP
+    </footer>
+    
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
 </html>
---
+++ php-web/aula6.php
@@ -0,0 +1,7 @@
+<?php include '../layouts/head.php'; ?>
+
+    <div class="container m-5">
+        <h1 class="text-center">Teste</h1>
+    </div>
+
+<?php include '../layouts/rodape.php' ?>
