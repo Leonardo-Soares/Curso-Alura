33df32071bf6e3ccca7d54f2bdd57d1b76112ac4
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-01-15 14:01:33 -0300

    Estilo utilizando JS e CSS

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Curso-Alura
-Cursos, Projetos e Trilhas de Aprendizado Alura
+# Aula 3.2 - Estilos com JavaScript
+
+- Comandos CSS com traços devem ser separados por letras maiúscula.
+    Exemplo: variavel.style.backgroundColor = "nomecor";
+
+- Referênciando uma Class do CSS
+    Exemplo: variavel.classList.add("nomeclass");
+
--- js/script.js
+++ js/script.js
@@ -1,43 +1,46 @@
 var titulo_menu = document.querySelector(".titulo-menu");
 titulo_menu.textContent = "Meus Pacientes";
 
-// BUSCA DE 1 PACIENTE
-var paciente = document.querySelector("#primeiro-paciente");
-var titulopaciente = paciente.querySelector(".info-nome");
-var tdpeso = paciente.querySelector(".info-peso");
-var tdaltura = paciente.querySelector(".info-altura");
-var tdImc = paciente.querySelector(".info-imc");
+var pacientes = document.querySelectorAll(".paciente");
 
-var paciente = titulopaciente.textContent;
-var peso = tdpeso.textContent; // Vai pegar o conteudo da váriavel
-var altura = tdaltura.textContent; // Vai pegar o conteudo da váriavel
+for(var conta = 0; conta < pacientes.length; conta++){
 
-validaPeso = true;
-validaAltura = true;
+    var paciente = pacientes[conta]; //Passa o array com o id de cada paciente
 
-// Validação 
-if (peso <= 0 || peso >= 1000) {
-    validaPeso = false;
-    tdImc.textContent = "Peso inválido";
-}
+    var titulopaciente = paciente.querySelector(".info-nome");
+    var tdpeso = paciente.querySelector(".info-peso");
+    var tdaltura = paciente.querySelector(".info-altura");
+    var tdImc = paciente.querySelector(".info-imc");
 
-else if (altura <= 0 || altura >= 3.05) {
-    validaAltura = false;
-    tdImc.textContent = "Altura inválida";
-}
+    var paciente = titulopaciente.textContent;
+    var peso = tdpeso.textContent; // Vai pegar o conteudo da váriavel
+    var altura = tdaltura.textContent; // Vai pegar o conteudo da váriavel
 
-else if (validaPeso == false && validaAltura == false) {
-    tdImc.textContent = "Altura e peso inválidos!"
-}
+    validaPeso = true;
+    validaAltura = true;
 
-else if (validaPeso && validaAltura) {
-    var imc = (peso / (altura * altura)); // Calcula o IMC
+    // Validação 
+    if (peso <= 0 || peso >= 1000) {
+        validaPeso = false;
+        tdImc.textContent = "Peso inválido";
+        tdImc.classList.add("dado-invalido")
+    }
 
-    tdImc.textContent = imc; // Exibi imc na table
-}
+    else if (altura <= 0 || altura >= 3.00) {
+        validaAltura = false;
+        tdImc.textContent = "Altura inválida";
+        tdImc.classList.add("dado-invalido")
+    }
 
-else {
-    tdImc.textContent = "Erro";
+    else if (validaPeso && validaAltura) {
+        var imc = (peso / (altura * altura)); // Calcula o IMC
+        tdImc.textContent = imc.toFixed(2); // Exibi imc na table
+    }
+
+    else {
+        tdImc.textContent = "Erro";
+    }
+
 }
 
 
