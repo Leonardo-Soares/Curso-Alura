0532eb5a189397fc7348e57950d80ddf39fa7c82
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-01-15 13:38:14 -0300

    Cálculo do IMC para 1 paciente

--- js/script.js
+++ js/script.js
@@ -1,21 +1,48 @@
 var titulo_menu = document.querySelector(".titulo-menu");
 titulo_menu.textContent = "Meus Pacientes";
 
+// BUSCA DE 1 PACIENTE
 var paciente = document.querySelector("#primeiro-paciente");
-console.log(paciente);
+var titulopaciente = paciente.querySelector(".info-nome");
+var tdpeso = paciente.querySelector(".info-peso");
+var tdaltura = paciente.querySelector(".info-altura");
+var tdImc = paciente.querySelector(".info-imc");
+
+var paciente = titulopaciente.textContent;
+var peso = tdpeso.textContent; // Vai pegar o conteudo da váriavel
+var altura = tdaltura.textContent; // Vai pegar o conteudo da váriavel
+
+validaPeso = true;
+validaAltura = true;
+
+// Validação 
+if (peso <= 0 || peso >= 1000) {
+    validaPeso = false;
+    tdImc.textContent = "Peso inválido";
+}
+
+else if (altura <= 0 || altura >= 3.05) {
+    validaAltura = false;
+    tdImc.textContent = "Altura inválida";
+}
+
+else if (validaPeso == false && validaAltura == false) {
+    tdImc.textContent = "Altura e peso inválidos!"
+}
+
+else if (validaPeso && validaAltura) {
+    var imc = (peso / (altura * altura)); // Calcula o IMC
+
+    tdImc.textContent = imc; // Exibi imc na table
+}
+
+else {
+    tdImc.textContent = "Erro";
+}
 
 
-// var cliente = document.querySelector("#primeiro-paciente");
-// var tdpeso = cliente.querySelector(".info-peso");
-// var peso = tdpeso.textContent;
-// var tdaltura = cliente.querySelector(".info-altura");
-// var altura = tdaltura.textContent;
-
-var imc = peso / (altura * altura);
 
 
-console.log(cliente);
-console.log(peso);
-console.log(altura);
 
-console.log(imc);
+
+
