c47ab2626f6e9b917cbe3181d4eeb683c254301a
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-01-15 10:45:56 -0300

    Eliminação do MVC

--- js/components.js
+++
@@ -1,6 +0,0 @@
-
-$(function(){
-  $("#includedContent").load("components/cabecario.html"); 
-  $("#aula").load("views/aula1.html"); 
-  $("#footer").load("components/footer.html");
-});
--- js/script.js
+++ js/script.js
@@ -1,34 +1,15 @@
-// IMPRESSÃO NO CONSOLE DO NAVEGADOR 
-//console.log('teste console'); 
-//console.log(document);
-
-// SELETOR
-//var exibir = document.querySelector("h1");
-//console.log(exibir);
-
 var titulo_menu = document.querySelector(".titulo-menu");
-titulo_menu.textContent = "Alessandra Monteiro";
+titulo_menu.textContent = "Meus Pacientes";
 
-var url_atual = window.location.href;
-
-
-if (url_atual == '%','index.html'){
-    console.log('essa');
-} else if (url_atual == '%','sobre.html') {
-    console.log('outra');
-} else {
-    console.log('erro');
-}
-
-console.log(url_atual);
+var paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente);
 
 
-
-var cliente = document.querySelector("#primeiro-paciente");
-var tdpeso = cliente.querySelector(".info-peso");
-var peso = tdpeso.textContent;
-var tdaltura = cliente.querySelector(".info-altura");
-var altura = tdaltura.textContent;
+// var cliente = document.querySelector("#primeiro-paciente");
+// var tdpeso = cliente.querySelector(".info-peso");
+// var peso = tdpeso.textContent;
+// var tdaltura = cliente.querySelector(".info-altura");
+// var altura = tdaltura.textContent;
 
 var imc = peso / (altura * altura);
 
